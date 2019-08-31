490e63e77d099c03e7ff3e6350c6be3f76377482
Xh4H <20613820+Xh4H@users.noreply.github.com>
2019-08-31 17:12:05 +0200

    Added youtube icons

--- preload.js
+++ preload.js
@@ -1,9 +1,13 @@
 // Custom events
 const challenge_opener = require("./events/challenge_opener");
 const machines_discord = require("./events/machines_discord");
+const machines_youtube = require("./events/machines_youtube");
 const shoutbox = require("./events/shoutbox");
 
-let actions = [shoutbox, challenge_opener, machines_discord];
+// window configs
+const ippsec_videos = require("./custom_dependencies/ippsec_videos");
+
+let actions = [shoutbox, challenge_opener, machines_discord, machines_youtube];
 
 function startActions(url) {
 	actions.forEach(action => action(url));
@@ -11,6 +15,7 @@ function startActions(url) {
 
 window.onload = function() {
 	let url = new URL(window.location.href);
+	window.ippsec = ippsec_videos;
 	window.apiToken = window.Laravel.apiToken;
 	startActions(url);
 };
