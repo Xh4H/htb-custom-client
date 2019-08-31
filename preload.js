859a7b251794a989b577675f491a0a2b818f77ae
Xh4H <20613820+Xh4H@users.noreply.github.com>
2019-08-31 18:36:08 +0200

    Added hide feature

--- preload.js
+++ preload.js
@@ -3,11 +3,12 @@ const challenge_opener = require("./events/challenge_opener");
 const machines_discord = require("./events/machines_discord");
 const machines_youtube = require("./events/machines_youtube");
 const shoutbox = require("./events/shoutbox");
+const hide = require("./events/hide");
 
 // window configs
 const ippsec_videos = require("./custom_dependencies/ippsec_videos");
 
-let actions = [shoutbox, challenge_opener, machines_discord, machines_youtube];
+let actions = [shoutbox, challenge_opener, machines_discord, machines_youtube, hide];
 
 function startActions(url) {
 	actions.forEach(action => action(url));
