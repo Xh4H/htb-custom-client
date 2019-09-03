624beab352e273f2f976aa72a1daa35d37faa016
Xh4H <20613820+Xh4H@users.noreply.github.com>
2019-09-03 16:11:21 +0200

    Now ippsec videos get loaded dinamically

--- preload.js
+++ preload.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+let req = axios.get("https://raw.githubusercontent.com/Xh4H/htb-custom-client/master/custom_dependencies/ippsec_videos.json");
+
 // Custom events
 const challenge_opener = require("./events/challenge_opener");
 const machines_discord = require("./events/machines_discord");
@@ -6,8 +9,6 @@ const shoutbox = require("./events/shoutbox");
 const hide = require("./events/hide");
 
 // window configs
-const ippsec_videos = require("./custom_dependencies/ippsec_videos");
-
 let actions = [shoutbox, challenge_opener, machines_discord, machines_youtube, hide];
 
 function startActions(url) {
@@ -16,7 +17,10 @@ function startActions(url) {
 
 window.onload = function() {
 	let url = new URL(window.location.href);
-	window.ippsec = ippsec_videos;
 	window.apiToken = window.Laravel.apiToken;
+
 	startActions(url);
+	req.then(response => {
+		window.ippsec = response.data;
+	})
 };
